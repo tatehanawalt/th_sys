27e407afbccded2cdff06ed4b489987337fe4c85
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-18 12:44:16 -0500

    fixing zsh

--- demo1/demo1.zsh
+++ demo1/demo1.zsh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
 
-printf "\n\n This is an upgraded and folder isolated HEAD brew script!\n\n"
+demo1() {
+  printf "\n\n This is an upgraded and folder isolated HEAD brew script!\n\n"
+}
+
+demo1 "$@"
