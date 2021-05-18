2d5953d3c09d5f68eb046dda10b5b9750ff324ec
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-18 11:48:17 -0500

    adding compile time version

--- demo2/main.go
+++ demo2/main.go
@@ -4,6 +4,11 @@ import (
   "fmt"
 )
 
+var Version = "devel"
+
 func main() {
-  fmt.Printf("\nDEMO 2\n\n")
+  fmt.Printf("\nDEMO 2\n")
+  fmt.Printf("VERSION=%v\n", Version)
+
+  fmt.Printf("\n")
 }
