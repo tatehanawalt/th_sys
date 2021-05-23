7a39d206baac08f3cd3d5d4904c45db1131cb7d4
Tate Hanawalt <tate@tatehanawalt.com>
2021-05-23 15:27:57 -0500

    store commit

--- devenv/scripts/devinfo.zsh
+++ devenv/scripts/devinfo.zsh
@@ -1,3 +1,19 @@
 #!/usr/bin/env zsh
 
-printf "\n TAP:\n DEV ENVIRONMENT:\n\n"
+printf "\n\n"
+printf "LOCAL:\n"
+printf "DEV ENVIRONMENT:\n\n"
+
+printf "Keg - formula namespace (found in /usr/local/Cellar)\n\n"
+
+output_lines=(${(@f)"$(ls -l /usr/local/opt)"})
+
+printf " - %s\n" $output_lines
+
+printf "\n"
+
+printf "FORMULA:\n"
+printf "\t- API REF:  https://rubydoc.brew.sh/Formula.html\n"
+printf "\t- COOKBOOK: https://docs.brew.sh/Formula-Cookbook\n"
+
+printf "\n"
